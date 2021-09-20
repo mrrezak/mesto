cddf543dcbf1077c3d0ed495f7a12f22f05c6f4f
mrrezak <87383789+mrrezak@users.noreply.github.com>
2021-09-20 16:19:05 +0300

    second_rev

--- script/script.js
+++ script/script.js
@@ -1,65 +1,67 @@
-let root = document.querySelector(".root");
-let popupOp = root.querySelector(".profile__open");
-let popup = root.querySelector(".popup");
-let formElement = root.querySelector(".form");
+let popupOpen = document.querySelector(".profile__open");
+let popup = document.querySelector(".popup");
+let formElement = document.querySelector(".form");
 let nameInput = formElement.querySelector("#title");
+let closeButton = document.querySelector(".popup__close");
 let jobInput = formElement.querySelector("#subtitle");
-let myLike = root.querySelector(".grid__like");
+let myLike = document.querySelector(".grid__like");
 
-let profN = root.querySelector(".profile__name");
-let jobN = root.querySelector(".profile__major");
+let profileName = document.querySelector(".profile__name");
+let jobName = document.querySelector(".profile__major");
 
-root.addEventListener("click", closePopup);
-root.addEventListener("click", active);
+//document.addEventListener("click", closePopup);
+//document.addEventListener("click", active);
 
-popupOp.addEventListener("click", function () {
-  setPopupInputValue();
-  openPopup(popup);
-});
 
 function openPopup(modal) {
-  console.log(modal);
+  nameInput.value = profileName.textContent;
+  jobInput.value = jobName.textContent;
   modal.classList.add("popup_visible");
 }
 
-function active(modal) {
-  const doLike = modal.target;
-  console.log(doLike);
-
-  if (
-    doLike.classList.contains("grid__like") &&
-    doLike.classList.contains("grid__like_active") != true
-  ) {
-    doLike.classList.add("grid__like_active");
-  } else {
-    doLike.classList.remove("grid__like_active");
-  }
-}
+//function active(modal) {
+//  const doLike = modal.target;
+//  console.log(doLike);
+//
+//  if (
+//    doLike.classList.contains("grid__like") &&
+//    doLike.classList.contains("grid__like_active") != true
+//  ) {
+//    doLike.classList.add("grid__like_active");
+// } else {
+//    doLike.classList.remove("grid__like_active");
+//  }
+//}
 
 function closePopup(evt) {
-  const closeBtn = evt.target;
-
-  if (
-    closeBtn.classList.contains("popup__close-image") ||
-    closeBtn.classList.contains("form__button")
-  ) {
-    closeBtn.closest(".popup").classList.remove("popup_visible");
-  }
+  evt.classList.remove('popup_visible')
 }
 
-function setPopupInputValue() {
-  nameInput.value = profN.textContent;
-  jobInput.value = jobN.textContent;
+function closePopupButtonClick(evt) {
+  const target = evt.target;
+  const currentPopup = target.closest('.popup')
+  closePopup(currentPopup)
 }
+
+//function setPopupInputValue() {
+//  nameInput.value = profileName.textContent;
+//  jobInput.value = jobName.textContent;
+//}
 
 function setNodeTextValue() {
-  profN.textContent = nameInput.value;
-  jobN.textContent = jobInput.value;
+  profileName.textContent = nameInput.value;
+  jobName.textContent = jobInput.value;
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   setNodeTextValue();
+  closePopup(popup)
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
+closeButton.addEventListener("click", closePopupButtonClick);
+popupOpen.addEventListener("click", function () {
+  //setPopupInputValue();
+  openPopup(popup);
+});
