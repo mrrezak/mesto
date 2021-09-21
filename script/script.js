e66349ce8f2118daa6c872b34f636d2022fca833
mrrezak <87383789+mrrezak@users.noreply.github.com>
2021-09-21 23:36:31 +0300

    4_rev

--- script/script.js
+++ script/script.js
@@ -1,38 +1,30 @@
-let popupOpen = document.querySelector(".profile__open");
-let popup = document.querySelector(".popup");
-let formElement = document.querySelector(".form");
-let nameInput = formElement.querySelector("#title");
-let closeButton = document.querySelector(".popup__close");
-let jobInput = formElement.querySelector("#subtitle");
+let popupOpen = document.querySelector('.profile__open');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.form');
+let nameInput = formElement.querySelector('#title');
+let closeButton = document.querySelector('.popup__close');
+let jobInput = formElement.querySelector('#subtitle');
 
-let profileName = document.querySelector(".profile__name");
-let jobName = document.querySelector(".profile__major");
+let profileName = document.querySelector('.profile__name');
+let jobName = document.querySelector('.profile__major');
 
 function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = jobName.textContent;
-  popup.classList.add("popup_visible");
+  popup.classList.add('popup_visible');
 }
 
 function closePopup() {
-  popup.classList.remove("popup_visible");
-}
-
-function closePopupButtonClick(evt) {
-  closePopup(popup);
-}
-
-function setNodeTextValue() {
-  profileName.textContent = nameInput.value;
-  jobName.textContent = jobInput.value;
+  popup.classList.remove('popup_visible');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  setNodeTextValue();
-  closePopup(popup);
+  profileName.textContent = nameInput.value;
+  jobName.textContent = jobInput.value;
+  closePopup();
 }
 
-formElement.addEventListener("submit", formSubmitHandler);
-closeButton.addEventListener("click", closePopupButtonClick);
-popupOpen.addEventListener("click", openPopup);
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', closePopup);
+popupOpen.addEventListener('click', openPopup);
