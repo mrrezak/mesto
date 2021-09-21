8f541c3b142d4e8a1f1f98d6c93de448ae4b53b5
mrrezak <87383789+mrrezak@users.noreply.github.com>
2021-09-21 11:31:26 +0300

    second_rev

--- script/script.js
+++ script/script.js
@@ -4,48 +4,24 @@ let formElement = document.querySelector(".form");
 let nameInput = formElement.querySelector("#title");
 let closeButton = document.querySelector(".popup__close");
 let jobInput = formElement.querySelector("#subtitle");
-let myLike = document.querySelector(".grid__like");
 
 let profileName = document.querySelector(".profile__name");
 let jobName = document.querySelector(".profile__major");
 
-//document.addEventListener("click", closePopup);
-//document.addEventListener("click", active);
 
-function openPopup(modal) {
+function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = jobName.textContent;
-  modal.classList.add("popup_visible");
+  popup.classList.add("popup_visible");
 }
 
-//function active(modal) {
-//  const doLike = modal.target;
-//  console.log(doLike);
-//
-//  if (
-//    doLike.classList.contains("grid__like") &&
-//    doLike.classList.contains("grid__like_active") != true
-//  ) {
-//    doLike.classList.add("grid__like_active");
-// } else {
-//    doLike.classList.remove("grid__like_active");
-//  }
-//}
-
-function closePopup(evt) {
-  evt.classList.remove("popup_visible");
+function closePopup() {
+  popup.classList.remove("popup_visible");
 }
 
 function closePopupButtonClick(evt) {
-  const target = evt.target;
-  const currentPopup = target.closest(".popup");
-  closePopup(currentPopup);
+  closePopup(popup);
 }
-
-//function setPopupInputValue() {
-//  nameInput.value = profileName.textContent;
-//  jobInput.value = jobName.textContent;
-//}
 
 function setNodeTextValue() {
   profileName.textContent = nameInput.value;
@@ -60,7 +36,4 @@ function formSubmitHandler(evt) {
 
 formElement.addEventListener("submit", formSubmitHandler);
 closeButton.addEventListener("click", closePopupButtonClick);
-popupOpen.addEventListener("click", function () {
-  //setPopupInputValue();
-  openPopup(popup);
-});
+popupOpen.addEventListener("click", openPopup);
